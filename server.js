01c7f62f3af4f6389aaf015ae404a4b5bdc443ab
Ari Bambang Kurniawan <aribk210@gmail.com>
2020-04-26 18:58:12 +0700

    setup database with mongoose

--- server.js
+++ server.js
@@ -3,10 +3,23 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 // app
 const app = express();
+
+// database
+const databaseName = process.env.DB_NAME;
+const databaseUser = process.env.DB_USER;
+const databasePassword = process.env.DB_PASSWORD;
+const databaseUrl = `mongodb+srv://${databaseUser}:${databasePassword}@blogs-wdfix.gcp.mongodb.net/${databaseName}?retryWrites=true&w=majority`;
+mongoose
+  .connect(databaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected.'));
 
 // middlewares
 app.use(morgan('dev'));
